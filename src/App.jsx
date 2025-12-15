0d569501b40a21b96222b7023f78658091305c26
Gongjjin <gjyj5395@naver.com>
2025-12-15 10:41:23 +0900

    feat: DreamPath 페이지 및 다국어 지원 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import FrienderPage from './components/FrienderPage';
+import DreamPathPage from './components/dreampath/DreamPathPage';
 
 function App() {
-  return <FrienderPage />;
+  return (
+    <Routes>
+      {/* 기존 Friender 페이지 */}
+      <Route path="/" element={<FrienderPage />} />
+      
+      {/* DreamPath 페이지 - 언어별 라우트 */}
+      <Route path="/dreampath/:language" element={<DreamPathPage />} />
+      
+      {/* 기본 경로는 Friender로 리다이렉트 */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
